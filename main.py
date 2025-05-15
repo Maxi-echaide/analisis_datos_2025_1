dd7f6c2be9a7c54fb163ae3fb57d3976005cd8d6
Miguel Puente <wilo1@pichu.cnbt.local>
2025-05-15 20:13:20 -0300

    resumen y base de datos

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from os import path
 from domain.dataset_csv import DatasetCSV
 from domain.dataset_excel import DatasetExcel
 from domain.dataset_api import DatasetAPI
+from data.data_saver import DataSaver
+
+
 # Ruta de archivos
 csv_path = path.join(path.dirname(__file__), "files/w_mean_prod.csv")
 excel_path = path.join(path.dirname(__file__), "files/ventas.xlsx")
@@ -9,11 +12,17 @@ excel_path = path.join(path.dirname(__file__), "files/ventas.xlsx")
 # Cargar y transformar
 csv = DatasetCSV(csv_path)
 csv.cargar_datos()
+csv.mostrar_resumen()
 
 excel = DatasetExcel(excel_path)
 excel.cargar_datos()
+excel.mostrar_resumen()
 
 api = DatasetAPI("https://apis.datos.gob.ar/georef/api/provincias")
-
+api.cargar_datos()
 
 # guardar en base de datos
+db = DataSaver("db/recoleccion.db")
+db.guardar_dataframe(csv.datos, "w_mean_prod_csv")
+db.guardar_dataframe(excel.datos, "ventas_xlsx")
+db.guardar_dataframe(api.datos, "provincia_api")
